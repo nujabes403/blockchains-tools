cb5172ecdacd1f04abef36275de96d662145b673
nujabes403 <nujabes403@gmail.com>
2019-02-06 21:22:08 +0900

    Add PrivateKeyToPublicKey component

--- src/components/PrivateKeyToPublicKey.js
+++ src/components/PrivateKeyToPublicKey.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import cx from 'classnames'
+import { fromEvent, merge } from 'rxjs'
+import { map, filter, tap } from 'rxjs/operators'
 import keccak256 from 'keccak256'
 
 import Input from 'components/Input'
@@ -12,33 +15,137 @@ type Props = {
 
 class PrivateKeyToPublicKey extends Component<Props> {
   state = {
-    privateKeyInstance: null,
-    publicKey: '',
     publicAddress: '',
+    focused: '',
+    changeTarget: {},
+    removeTarget: {},
   }
 
-  handleChange = (e) => {
-    const privateKey = e.target.value.replace('0x', '')
-    const privateKeyInstance = ec.keyFromPrivate(privateKey)
-    const publicKeyInstance = privateKeyInstance.getPublic()
-    const publicKey = '0x' + publicKeyInstance.getX().toString(16) + publicKeyInstance.getY().toString(16)
-    const publicAddress = '0x' + keccak256(publicKey).toString('hex').slice(-40)
-    this.setState({
-      publicKey,
-      publicAddress,
-    })
+  initInputChangeStreams = () => {
+    // Input Change Streams
+    const privateKeyChange$ = fromEvent(this.$privateKey, 'input').pipe(
+        map(e => e.target.value.replace('0x', ''))
+      )
+
+    const publicKeyChange$ = merge(
+      privateKeyChange$.pipe(
+        map(pvkey => {
+          try {
+            const publicKeyInstance = ec.keyFromPrivate(pvkey).getPublic()
+            const publicKey = '0x' + publicKeyInstance.getX().toString(16) + publicKeyInstance.getY().toString(16)
+            return publicKey
+          } catch (e) {
+            console.log(e, 'e')
+            return ''
+          }
+        })
+      ),
+      fromEvent(this.$publicKey, 'input').pipe(
+        tap(this.clearPrivateKey),
+        map(e => e.target.value),
+      )
+    )
+
+    publicKeyChange$.subscribe(pbkeyRaw => {
+        if (this.$publicKey.value !== pbkeyRaw) this.$publicKey.value = pbkeyRaw
+        const pbkey = pbkeyRaw.startsWith('0x') ? pbkeyRaw : '0x' + pbkeyRaw
+        this.setState({
+          publicAddress: '0x' + keccak256(pbkey).toString('hex').slice(-40),
+        })
+      })
+  }
+
+  initActiveStreams = () => {
+    const privateKeyFocus$ = fromEvent(this.$privateKey, 'focus')
+    const privateKeyMouseEnter$ = fromEvent(this.$privateKey, 'mouseenter')
+    const privateKeyActive$ = merge(privateKeyFocus$, privateKeyMouseEnter$).pipe(
+      tap(() => {
+        this.setState({
+          changeTarget: {
+            publicKey: true,
+            publicAddress: true,
+          }
+        })
+      })
+    )
+
+    const publicKeyFocus$ = fromEvent(this.$publicKey, 'focus')
+    const publicKeyMouseEnter$ = fromEvent(this.$publicKey, 'mouseenter')
+    const publicKeyActive$ = merge(publicKeyFocus$, publicKeyMouseEnter$).pipe(
+      tap(() => {
+        this.setState({
+          changeTarget: {
+            publicAddress: true,
+          },
+          removeTarget: {
+            privateKey: true,
+          }
+        })
+      })
+    )
+
+    privateKeyActive$.subscribe()
+    publicKeyActive$.subscribe()
+  }
+
+  initDeactiveStreams = () => {
+    const blur$ = merge(
+      fromEvent(this.$privateKey, 'blur'),
+      fromEvent(this.$publicKey, 'blur')
+    )
+    const mouseleave$ = merge(
+      fromEvent(this.$privateKey, 'mouseleave'),
+      fromEvent(this.$publicKey, 'mouseleave')
+    )
+
+    const deactive$ = merge(blur$, mouseleave$).pipe(
+      filter((e) => document.activeElement !== e.fromElement),
+      tap(() => {
+        this.setState({
+          changeTarget: {},
+          removeTarget: {},
+        })
+      })
+    )
+
+    deactive$.subscribe()
+  }
+
+  clearPrivateKey = () => {
+    this.$privateKey.value = ''
+  }
+
+  componentDidMount() {
+    this.initInputChangeStreams()
+    this.initActiveStreams()
+    this.initDeactiveStreams()
   }
 
   render() {
-    const { privateKeyInstance, publicKey, publicAddress } = this.state
+    const { publicAddress, changeTarget, removeTarget } = this.state
     return (
       <div className="PrivateKeyToPublicKey">
-        <Input
-          name="privateKey"
-          onChange={this.handleChange}
+        <input
+          className={cx('privateKey', {
+            'privateKey--changeTarget': changeTarget.privateKey,
+            'privateKey--removeTarget': removeTarget.privateKey,
+          })}
+          ref={($privateKey) => this.$privateKey = $privateKey}
         />
-        <p>public key: {publicKey}</p>
-        <p>public address: {publicAddress}</p>
+        <input
+          className={cx('publicKey', {
+            'publicKey--changeTarget': changeTarget.publicKey
+          })}
+          ref={($publicKey) => this.$publicKey = $publicKey}
+        />
+        <p
+          className={cx('publicAddress', {
+            'publicAddress--changeTarget': changeTarget.publicAddress,
+          })}
+          ref={($publicAddress) => this.$publicAddress = $publicAddress}
+        >
+          public address: {publicAddress}
+        </p>
       </div>
     )
   }
