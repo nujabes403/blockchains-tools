9e5b3e566a7428471120c4ce58950e7f994bd766
nujabes403 <nujabes403@gmail.com>
2019-11-07 22:36:32 +0900

    Add Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,86 @@
+import React, { Component, Fragment, createRef } from 'react'
+import cx from 'classnames'
+import { browserHistory } from 'react-router'
+import { Subject } from 'rxjs'
+import { takeUntil, tap } from 'rxjs/operators'
+
+import Ad from 'components/Ad'
+import { menuItems } from 'constants/menu'
+
+import './Sidebar.scss'
+
+type Props = {
+  
+}
+
+class Sidebar extends Component<Props> {
+  destroy$ = new Subject()
+
+  componentWillUnmount() {
+    this.destroy$.next(true)
+  }
+
+  render() {
+    const { query, pathname } = browserHistory.getCurrentLocation()
+    
+    const selectedItem = {
+      link: pathname,
+      pathnameOnly: String(pathname).split('?')[0],
+      label: query && query.l
+    }
+    
+    return (
+      <div className="Sidebar">
+        <div 
+          className={cx('Sidebar__label', {
+            'Sidebar__label--selected': query && !query.l,
+          })}
+          onClick={() => browserHistory.push('/')}
+        >
+          Welcome
+        </div>
+        {menuItems && Object.entries(menuItems).map(([bookLabel, items]) => {
+          
+          const isBookLabelSelected = selectedItem.label && (selectedItem.label === bookLabel)
+          
+          return (
+            <Fragment>
+              <div 
+                className={cx('Sidebar__label', {
+                  'Sidebar__label--selected': isBookLabelSelected,
+                })}
+                onClick={() => {
+                  if (items && items.length !== 0) {
+                    browserHistory.push(items[0].link)
+                  }
+                }}
+              >
+                {bookLabel}
+              </div>
+              {isBookLabelSelected && items.map(({ title, link }) => {
+                const itemPathnameOnly = String(link).split('?')[0]
+                const isItemSelected = isBookLabelSelected
+                  && selectedItem.pathnameOnly 
+                  && (selectedItem.pathnameOnly === itemPathnameOnly)
+                
+                return (
+                  <li 
+                    className={cx('Sidebar__link', {
+                      'Sidebar__link--selected': isItemSelected,
+                    })}
+                    onClick={() => browserHistory.push(link)}
+                  >
+                    {title}
+                  </li>
+                )
+              })}
+            </Fragment>
+          )
+        })}
+        <Ad />
+      </div>
+    )
+  }
+}
+
+export default Sidebar
